5f7c4fcdfa8b3eb72c7fd0c894067a9b64eb560b
Alexie McDonald <alexiemcdonald304@gmail.com>
2025-07-08 09:18:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
-morganBody(app, {logAllReqHeader:true, maxBodyLength:5000}
+morganBody(app, {logAllReqHeader:true, maxBodyLength:5000});
            
 app.get("/", (req, res) => res.type('html').send(html));
 
